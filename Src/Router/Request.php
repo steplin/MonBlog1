b42463c71c90cc3227f97c899fd79a2c47e985aa
BRIERE Stéphane <stephanebriere@hotmail.fr>
2018-08-27 21:39:30 +0200

    Fixes of the Request class

--- Src/Router/Request.php
+++ Src/Router/Request.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Router;
 
 /**
@@ -9,72 +8,77 @@ namespace Router;
 class Request
 {
 
-    /**
-     *
-     * @param type $key
-     * @return type
-     */
-    public function getData($key)
-    {
-        return isset($_GET[$key]) && !empty($_GET[$key]) ? $_GET[$key] : null;
-    }
+	/**
+	 *
+	 * @param type $key
+	 * @return type
+	 */
+	public function getData($key)
+	{
+		$getKey = $_GET[$key];
+		return isset($getKey) &&!empty($getKey = ) ? $getKey = : null;
+	}
 
-    /**
-     *
-     * @return type
-     */
-    public function method()
-    {
-        return $_SERVER['REQUEST_METHOD'];
-    }
+	/**
+	 *
+	 * @return type
+	 */
+	public function method()
+	{
+		$serverMethod = $_SERVER['REQUEST_METHOD'];
+		return $serverMethod;
+	}
 
-    /**
-     *
-     * @param type $key
-     * @return type
-     */
-    public function postData($key)
-    {
-        return isset($_POST[$key])
-            && !empty($_POST[$key]) ? $_POST[$key] : null;
-    }
+	/**
+	 *
+	 * @param type $key
+	 * @return type
+	 */
+	public function postData($key)
+	{
+		$postKey = $_POST[$key];
+		return isset($postKey) && !empty($postKey) ? $postKey : null;
+	}
 
-    /**
-     *
-     * @param type $key
-     * @return type
-     */
-    public function postExists($key)
-    {
-        return isset($_POST[$key]);
-    }
+	/**
+	 *
+	 * @param type $key
+	 * @return type
+	 */
+	public function postExists($key)
+	{
+		$postKey = $_POST[$key];
+		return isset($postKey);
+	}
 
-    /**
-     *
-     * @return type
-     */
-    public function requestURI()
-    {
-        return $_SERVER['REQUEST_URI'];
-    }
+	/**
+	 *
+	 * @return type
+	 */
+	public function requestURI()
+	{
+		$serverRequest = $_SERVER['REQUEST_URI'];
+		return $serverRequest;
+	}
 
-    /**
-     *
-     * @return type
-     */
-    public function requestURL()
-    {
-        // return $_SERVER['REDIRECT_SCRIPT_URL']; en ligne
-        return $_SERVER['REDIRECT_URL'];
-    }
+	/**
+	 *
+	 * @return type
+	 */
+	public function requestURL()
+	{
+		// return $_SERVER['REDIRECT_SCRIPT_URL']; en ligne
+		$serverUrl = $_SERVER['REDIRECT_URL'];
+		return $serverUrl;
+	}
 
-    /**
-     *
-     * @param type $key
-     * @param type $value
-     */
-    public function setGet($key, $value)
-    {
-        $_GET[$key] = $value;
-    }
+	/**
+	 *
+	 * @param type $key
+	 * @param type $value
+	 */
+	public function setGet($key, $value)
+	{
+		$_GET[$key] = $value;
+	}
 }
